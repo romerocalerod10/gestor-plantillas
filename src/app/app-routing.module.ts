7c41e50af10fbd0a50546ddf5ab727006779a61a
DANIEL ROMERO CALERO <Daniel.RomeroCalero@gecolabor.com>
2024-05-17 14:01:54 +0100

    Añadido routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Ruta raíz que carga el componente HomeComponent
+  // Puedes agregar más rutas aquí según sea necesario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
